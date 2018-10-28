20426ea9f0a2d4faff4b56c3e85d06c0b8f1f3a5
Kinney凯 <kinneyzhang666@gmail.com>
2018-10-28 23:42:25 +0800

    write package-configs into myconfig.org and updating now...

--- init.el
+++ init.el
@@ -1,26 +1,41 @@
-;; Added export PATH="/path/to/code/cask/bin:$PATH" by Package.el.  This must come before configurations of
+;; Added export PATH="/path/to/code/cask/bin:$PATH" by Package.el.
+;; This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.emacs-china.org/gnu/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://elpa.emacs-china.org/melpa/"))
 (package-initialize)
 
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myconfig.org"))
+
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
-
-;;add more personal funcs
 (require 'packages-manage)
-(require 'init-packages)
-(require 'init-ui)
-(require 'init-better-defaults)
-(require 'init-org)
-(require 'init-keybindings)
 
-(setq custom-file (expand-file-name "lisp/init-custom.el" user-emacs-directory))
-(load-file custom-file)
 
-(setq org-agenda-files '("~/org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes
+   (quote
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+ '(evil-leader/leader "<SPC>"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
