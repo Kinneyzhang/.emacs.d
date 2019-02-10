8020610f77bbda8688b6a99a79482b5c1c299594
Kinney凯 <kinneyzhang666@gmail.com>
2019-02-10 17:36:57 +0800

    add 'print-special-symbol'functions

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
+(read-only-mode 1)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myconfig.org"))
 
 
@@ -30,10 +31,10 @@
  '(ansi-color-names-vector
    (vector "#424242" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#eaeaea"))
  '(beacon-color "#cc6666")
- '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
+ '(custom-enabled-themes (quote (leuven)))
  '(custom-safe-themes
    (quote
-    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+    ("43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(dashboard-image-banner-max-height 20)
  '(dashboard-image-banner-max-width 20)
  '(evil-leader/leader ";")
@@ -87,7 +88,4 @@
  ;; If there is more than one, they won't work right.
  '(dashboard-banner-logo-title-face ((t (:inherit default :foreground "#65C4EB"))))
  '(dashboard-heading-face ((t (:inherit default :foreground "#65C4EB"))))
- '(org-block ((t (:inherit shadow :foreground "white"))))
- '(web-mode-html-attr-name-face ((t (:foreground "#FF9900"))))
- '(web-mode-html-tag-bracket-face ((t (:foreground "#408CFF"))))
- '(web-mode-html-tag-face ((t (:foreground "#408CFF")))))
+ '(org-block ((t (:inherit shadow :background "#FFFFE0" :foreground "default")))))
