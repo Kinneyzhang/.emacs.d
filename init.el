80397302b4395d677804f7d4b361cd567a2a730d
Kinney凯 <kinneyzhang666@gmail.com>
2019-06-17 23:20:05 +0800

    use xwidgets-webkit

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
 	     '("gnu"   . "http://elpa.emacs-china.org/gnu/"))
 (add-to-list 'package-archives
 	     '("melpa" . "http://elpa.emacs-china.org/melpa/"))
-(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
@@ -35,32 +34,29 @@
  '(beacon-color "#cc6666")
  '(bongo-default-directory "~/Music")
  '(bongo-display-inline-playback-progress nil)
- '(bongo-enabled-backends (quote (mplayer afplay)))
+ '(bongo-enabled-backends '(mplayer afplay))
  '(cfw:display-calendar-holidays nil)
  '(cfw:org-capture-template
-   (quote
-    ("c" "calfw2org" entry
+   '("c" "calfw2org" entry
      (file nil)
      "* %?
-    %(cfw:org-capture-day)")))
+    %(cfw:org-capture-day)"))
  '(column-number-mode t)
- '(custom-enabled-themes (quote (doom-one)))
+ '(custom-enabled-themes '(doom-one))
  '(custom-safe-themes
-   (quote
-    ("93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+   '("93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
  '(dashboard-image-banner-max-height 250)
  '(dashboard-image-banner-max-width 250)
  '(evil-leader/leader ";")
  '(fancy-battery-show-percentage t)
  '(fancy-splash-image "~/.emacs.d/img/ying.jpg")
  '(fci-rule-color "#373b41")
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(frame-background-mode (quote dark))
- '(hl-paren-colors (quote ("Cyan" "Gold" "Red")))
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
+ '(hl-paren-colors '("Cyan" "Gold" "Red"))
  '(hl-sexp-background-color "#efebe9")
  '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#dc752f")
+   '(("TODO" . "#dc752f")
      ("NEXT" . "#dc752f")
      ("THEM" . "#2d9574")
      ("PROG" . "#4f97d7")
@@ -75,7 +71,7 @@
      ("FIXME" . "#dc752f")
      ("XXX" . "#dc752f")
      ("XXXX" . "#dc752f")
-     ("???" . "#dc752f"))))
+     ("???" . "#dc752f")))
  '(imaxima-bg-color "black")
  '(imaxima-equation-color "Green3")
  '(imaxima-fg-color "DarkGreen")
@@ -89,39 +85,36 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#0d0f11" "#A8CE93"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#0d0f11" "#899BA6"))
  '(menu-bar-mode nil)
- '(neo-cwd-line-style (quote button))
+ '(neo-cwd-line-style 'button)
  '(neo-show-slash-for-folder nil)
- '(neo-theme (quote icons))
+ '(neo-theme 'icons)
  '(neo-window-fixed-size nil)
- '(neo-window-position (quote left))
+ '(neo-window-position 'left)
  '(neo-window-width 28)
- '(org-agenda-files (quote ("/Users/kinney/org/gtd.org")))
- '(org-agenda-window-setup (quote current-window))
+ '(org-agenda-files '("/Users/kinney/org/gtd.org"))
+ '(org-agenda-window-setup 'current-window)
  '(org-journal-dir "~/org/entries")
  '(org-pomodoro-ask-upon-killing nil)
  '(package-selected-packages
-   (quote
-    (eshell-prompt-extras alarm-clock move-text link-hint indent-guide helpful golden-ratio fancy-battery elscreen diredfl column-enforce-mode clean-aindent-mode buffer-move browse-at-remote beacon podcaster lsp-python-ms emms geiser wanderlust proxy-mode sudoku 2048-game bongo counsel-projectile ivy-rich all-the-icons-ivy restart-emacs howm deft markdown-preview-mode lsp-ui company-lsp org-notebook doom-modeline org-gcal graphql furl calfw calfw-org material-theme doom-themes search-web guess-word emmet-mode neotree all-the-icons-dired spacemacs-theme magit smex cnfonts chinese-fonts-setup youdao-dictionary dashboard elfeed-goodies elfeed django-mode alert async auto-complete auto-yasnippet company counsel dash epl flycheck hungry-delete iedit ivy js2-mode js2-refactor nodejs-repl org-projectile org-bullets package-build pkg-info popup popwin reveal-in-osx-finder ruby-hash-syntax smartparens swiper use-package web-mode which-key window-numbering yasnippet sr-speedbar)))
- '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
+   '(xwidgets alarm-clock move-text link-hint indent-guide helpful golden-ratio fancy-battery elscreen diredfl column-enforce-mode buffer-move browse-at-remote beacon podcaster lsp-python-ms emms geiser wanderlust proxy-mode sudoku 2048-game bongo counsel-projectile ivy-rich all-the-icons-ivy restart-emacs howm deft markdown-preview-mode lsp-ui company-lsp org-notebook doom-modeline org-gcal graphql furl calfw calfw-org material-theme doom-themes search-web guess-word emmet-mode neotree all-the-icons-dired spacemacs-theme magit smex cnfonts chinese-fonts-setup youdao-dictionary dashboard elfeed-goodies elfeed django-mode alert async auto-complete auto-yasnippet company counsel dash epl flycheck hungry-delete iedit ivy js2-mode js2-refactor nodejs-repl org-projectile org-bullets package-build pkg-info popup popwin reveal-in-osx-finder ruby-hash-syntax smartparens swiper use-package web-mode which-key window-numbering yasnippet sr-speedbar))
+ '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
  '(podcaster-feeds-urls
-   (quote
-    ("http://podcast-beta.miao.li/xml/netease/340909068/rss.xml" "https://kernelpanic.fm/feed")))
+   '("http://podcast-beta.miao.li/xml/netease/340909068/rss.xml" "https://kernelpanic.fm/feed"))
  '(podcaster-mp3-player "itunes")
  '(popwin-mode t)
  '(popwin:adjust-other-windows nil)
- '(proxy-mode-socks-proxy (quote ("Default server" "124.156.188.197" 3344 5)))
+ '(proxy-mode-socks-proxy '("Default server" "124.156.188.197" 3344 5))
  '(pyim-page-length 8)
- '(search-web-default-browser (quote browse-url-default-macosx-browser))
+ '(search-web-default-browser 'browse-url-default-browser)
  '(search-web-engines
-   (quote
-    (("github" "https://github.com/search?q=%s" nil)
+   '(("github" "https://github.com/search?q=%s" nil)
      ("baidu" "https://www.baidu.com/s?ie=UTF-8&wd=%s" nil)
      ("google" "http://www.google.com/search?q=%s" nil)
-     ("youtube" "http://www.youtube.com/results?search_type=&search_query=%s&aq=f" nil))))
- '(search-web-external-browser (quote browse-url-default-macosx-browser))
+     ("youtube" "http://www.youtube.com/results?search_type=&search_query=%s&aq=f" nil)))
+ '(search-web-external-browser 'browse-url-default-browser)
  '(show-paren-mode t)
- '(smiley-style (quote medium))
- '(speedbar-default-position (quote right))
+ '(smiley-style 'medium)
+ '(speedbar-default-position 'right)
  '(speedbar-show-unknown-files t)
  '(sr-speedbar-auto-refresh t)
  '(sr-speedbar-default-width 20)
@@ -134,8 +127,7 @@
  '(tramp-verbose 0 nil (tramp))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#cc6666")
+   '((20 . "#cc6666")
      (40 . "#de935f")
      (60 . "#f0c674")
      (80 . "#b5bd68")
@@ -152,7 +144,7 @@
      (300 . "#cc6666")
      (320 . "#de935f")
      (340 . "#f0c674")
-     (360 . "#b5bd68"))))
+     (360 . "#b5bd68")))
  '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -343,7 +335,7 @@
  '(speedbar-highlight-face ((((class color) (background dark)) (:background "LightGoldenrod" :foreground "black"))))
  '(speedbar-selected-face ((((class color) (background dark)) (:foreground "Cyan" :underline t))))
  '(speedbar-separator-face ((((class color) (background dark)) (:background "DarkRed" :foreground "white" :overline "gray"))))
- '(top-mode-mark-face (quote isearch))
+ '(top-mode-mark-face 'isearch)
  '(vr/group-0 ((t (:background "red" :foreground "black"))))
  '(vr/group-1 ((t (:background "gold2" :foreground "black"))))
  '(vr/group-2 ((t (:background "dodgerblue" :foreground "black"))))
