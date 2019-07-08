6dade72105994a3254c680106a639e6d5878ed1a
Kinney凯 <kinneyzhang666@gmail.com>
2019-07-08 11:45:59 +0800

    split C,python,web,js,custom.el config from myconfig.el

---
+++ elisp/custom.el
@@ -0,0 +1,44 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1B2229" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#DFDFDF"])
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default))
+ '(fci-rule-color "#5B6268")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(package-selected-packages
+   '(youdao-dictionary ycmd xelb wttrin window-numbering which-key web-mode wanderlust use-package undo-tree tablist sudoku ssass-mode spotlight spacemacs-theme soundcloud smex smartparens shut-up search-web ruby-hash-syntax reveal-in-osx-finder restart-emacs pyenv-mode py-autopep8 proxy-mode project-persist-drawer powerline popwin podcaster pip-requirements pinboard-popular pfuture paredit pacmacs package-build org-projectile org-gcal org-bullets nyan-mode noflet nodejs-repl neotree multi-term move-text mmm-mode math-symbol-lists material-theme markdown-preview-mode magit lsp-ui lsp-python-ms link-hint libmpdee js2-refactor ivy-rich ivy-posframe indent-guide impatient-mode image+ iedit idle-org-agenda hydra hungry-delete helpful hackernews graphql goto-chg google-translate google-c-style ggtags geiser furl flycheck fancy-battery expand-region exec-path-from-shell esxml emmet-mode elpy edit-indirect dracula-theme doom-themes doom-modeline django-mode diredfl dashboard dash-docs counsel-world-clock counsel-projectile counsel-osx-app company-web company-lsp company-jedi company-c-headers company-box column-enforce-mode calfw-org calfw buffer-move browse-at-remote bongo beacon auto-yasnippet auto-complete anaconda-mode all-the-icons-ivy all-the-icons-dired ace-window ace-popup-menu ace-jump-mode ac-html-bootstrap ac-html-angular 2048-game))
+ '(vc-annotate-background "#282c34")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ elisp/lang-web.el
@@ -0,0 +1,134 @@
+(use-package web-mode
+  :ensure t
+  :bind (("C-c ]" . emmet-next-edit-point)
+         ("C-c [" . emmet-prev-edit-point)
+         ("C-c o b" . browse-url-of-file))
+  :mode
+  (("\\.js\\'" . web-mode)
+   ("\\.html?\\'" . web-mode)
+   ("\\.phtml?\\'" . web-mode)
+   ("\\.tpl\\.php\\'" . web-mode)
+   ("\\.[agj]sp\\'" . web-mode)
+   ("\\.as[cp]x\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode)
+   ("\\.vue\\'" . web-mode)
+   ("\\.jsx$" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2)
+
+  (setq web-mode-engines-alist
+	  '(("php"    . "\\.phtml\\'")
+	    ("blade"  . "\\.blade\\.")
+	    ("django"  . "\\.djhtml\\'")
+	    ("django"  . "\\.html?\\'")))
+
+
+   ;;change indent style
+    (defun my-toggle-web-indent ()
+      (interactive)
+      (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+	  (progn
+	    (setq js-indent-level (if (= js-indent-level 2) 4 2))
+	    (setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+      (if (eq major-mode 'web-mode)
+	  (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+		 (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+		 (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+      (if (eq major-mode 'css-mode)
+	  (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+
+      (setq indent-tabs-mode nil))
+
+    (global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+  
+
+  (add-hook 'web-mode-hook 'jsx-flycheck)
+
+  ;; highlight enclosing tags of the element under cursor
+  (setq web-mode-enable-current-element-highlight t)
+
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "jsx")
+        (let ((web-mode-enable-part-face nil))
+          ad-do-it)
+      ad-do-it))
+
+  (defun jsx-flycheck ()
+    (when (equal web-mode-content-type "jsx")
+      ;; enable flycheck
+      (flycheck-select-checker 'jsxhint-checker)
+      (flycheck-mode)))
+
+  ;; editing enhancements for web-mode
+  ;; https://github.com/jtkDvlp/web-mode-edit-element
+  
+  ;; (use-package web-mode-edit-element
+  ;;   :ensure nil
+  ;;   :config (add-hook 'web-mode-hook 'web-mode-edit-element-minor-mode))
+
+  ;; snippets for HTML
+  ;; https://github.com/smihica/emmet-mode
+  (use-package emmet-mode
+    :ensure t
+    :init (setq emmet-move-cursor-between-quotes t) ;; default nil
+    :bind (("C-j" . emmet-expand-line))
+    :diminish (emmet-mode . " e"))
+  (add-hook 'web-mode-hook 'emmet-mode)
+
+  (defun my-web-mode-hook ()
+    "Hook for `web-mode' config for company-backends."
+    (set (make-local-variable 'company-backends)
+         '((company-tern company-css company-web-html company-files))))
+  (add-hook 'web-mode-hook 'my-web-mode-hook)
+
+  ;; Enable JavaScript completion between <script>...</script> etc.
+  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+    "Set `tern-mode' based on current language before running company-tern."
+    (message "advice")
+    (if (equal major-mode 'web-mode)
+	(let ((web-mode-cur-language
+	       (web-mode-language-at-pos)))
+	  (if (or (string= web-mode-cur-language "javascript")
+		  (string= web-mode-cur-language "jsx"))
+	      (unless tern-mode (tern-mode))
+	    (if tern-mode (tern-mode -1))))))
+  (add-hook 'web-mode-hook 'company-mode)
+
+  ;; to get completion data for angularJS
+  (use-package ac-html-angular
+    :ensure t
+    :defer t)
+  ;; to get completion for twitter bootstrap
+  (use-package ac-html-bootstrap
+    :ensure t
+    :defer t)
+
+  ;; to get completion for HTML stuff
+  ;; https://github.com/osv/company-web
+  (use-package company-web
+    :ensure t)
+
+  (add-hook 'web-mode-hook 'company-mode))
+
+;; configure CSS mode company backends
+(use-package css-mode
+  :config
+  (defun my-css-mode-hook ()
+    (set (make-local-variable 'company-backends)
+         '((company-css company-dabbrev-code company-files))))
+  (add-hook 'css-mode-hook 'my-css-mode-hook)
+  (add-hook 'css-mode-hook 'company-mode))
+
+;; impatient mode - Live refresh of web pages
+;; https://github.com/skeeto/impatient-mode
+(use-package impatient-mode
+  :ensure t
+  :diminish (impatient-mode . " i")
+  :commands (impatient-mode))
+
+(provide 'lang-web)
