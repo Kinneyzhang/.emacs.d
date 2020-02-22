9a0dd170b8ea41f43fb56cdf2e10ebe34c5da45a
Kinney Zhang <kinneyzhang666@gmail.com>
2020-02-22 08:13:21 +0800

    delete url

--- config-file/org-clock-save.el
+++ config-file/org-clock-save.el
@@ -1,3 +1,2 @@
-;; org-clock-save.el - 192.168.0.101 at <2020-02-21 Fri 20:28>
-(setq org-clock-stored-resume-clock '("/Users/kinney/iCloud/org/project.org" . 5476))
+;; org-clock-save.el - Kinneys-MacBook.local at <2020-02-22 Sat 00:22>
 (setq org-clock-stored-history '(("/Users/kinney/iCloud/org/project.org" . 4675) ("/Users/kinney/iCloud/org/task.org" . 7088) ("/Users/kinney/iCloud/org/project.org" . 3888) ("/Users/kinney/iCloud/org/project.org" . 4817) ("/Users/kinney/iCloud/org/project.org" . 5418)))
--- elisp/init-utils.el
+++ elisp/init-utils.el
@@ -279,36 +279,36 @@ Return a new buffer or BUF with the code in it."
 (add-hook 'after-init-hook #'chunyang-scratch-restore)
 
 ;; 统一局域网下上传文件
-(require 'web-server)
-(ws-start
- '(((:GET . ".*") .
-    (lambda (request)
-      (with-slots (process headers) request
-        (ws-response-header proc 200 '("Content-type" . "text/html"))
-        (process-send-string
-         process
-         "\
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<h1>Upload File</h1>
-<form method='post' enctype='multipart/form-data'>
-  <input type='file' name='file'>
-  <input type='submit'>
-</form>
-"))))
-   ((:POST . ".*") .
-    (lambda (request)
-      (with-slots (process headers) request
-        (let-alist (assoc-default "file" headers)
-          (let ((out (make-temp-file "x-" nil .filename)))
-            (let ((coding-system-for-write 'binary))
-              (write-region .content nil out))
-            (message "[%s] saved %d bytes to %s"
-                     (current-time-string)
-                     (string-bytes .content)
-                     out)
-            (ws-response-header process 200 '("Content-type" . "text/plain"))
-            (process-send-string process (format "saved to %s\n" out))))))))
- 9008 nil :host "0.0.0.0")
+;; (require 'web-server)
+;; (ws-start
+;;  '(((:GET . ".*") .
+;;     (lambda (request)
+;;       (with-slots (process headers) request
+;;         (ws-response-header proc 200 '("Content-type" . "text/html"))
+;;         (process-send-string
+;;          process
+;;          "\
+;; <meta name='viewport' content='width=device-width, initial-scale=1'>
+;; <h1>Upload File</h1>
+;; <form method='post' enctype='multipart/form-data'>
+;;   <input type='file' name='file'>
+;;   <input type='submit'>
+;; </form>
+;; "))))
+;;    ((:POST . ".*") .
+;;     (lambda (request)
+;;       (with-slots (process headers) request
+;;         (let-alist (assoc-default "file" headers)
+;;           (let ((out (make-temp-file "x-" nil .filename)))
+;;             (let ((coding-system-for-write 'binary))
+;;               (write-region .content nil out))
+;;             (message "[%s] saved %d bytes to %s"
+;;                      (current-time-string)
+;;                      (string-bytes .content)
+;;                      out)
+;;             (ws-response-header process 200 '("Content-type" . "text/plain"))
+;;             (process-send-string process (format "saved to %s\n" out))))))))
+;;  9008 nil :host "0.0.0.0")
 
 ;;-----------------------------
 (add-hook 'focus-in-hook 'my/mac-switch-input-source)
