160ce3c103472cc5673c7af6bc636d792d1aea49
Kinneyzhang <kinneyzhang666@gmail.com>
2022-08-10 17:55:19 +0800

    update init-util

--- elisp/init-utils.el
+++ elisp/init-utils.el
@@ -1,4 +1,11 @@
 ;;; init-utils
+
+(defun gk-set-window-margins ()
+  (interactive)
+  (let* ((margin (string-to-number
+                  (completing-read "Input margin: " nil))))
+    (set-window-margins (selected-window) margin margin)))
+
 (defvar gk-chinese-weekdays '("周一" "周二" "周三" "周四" "周五" "周六" "周日"))
 (defvar gk-english-weekdays '("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"))
 (defun gk-convert-weekday-to-english ()
@@ -18,19 +25,26 @@
           (message "%s" eng-weekday)
           (replace-match eng-weekday t))))))
 
-(defun gk/deploy-gtd ()
+(defun gk/deploy-mygtd ()
   "Deploy geekblog."
   (interactive)
   (let ((display-buffer-alist
          '(("\\*Async Shell Command\\*" display-buffer-no-window))))
-    (async-shell-command "cd C:/Users/26289/GTD && git add . && git commit -m 'update' && git push")))
+    (async-shell-command "cd c:/Users/26289/Hackings/mygtd && git add . && git commit -m 'update' && git push")))
+
+(defun gk/deploy-docks ()
+  "Deploy geekblog."
+  (interactive)
+  (let ((display-buffer-alist
+         '(("\\*Async Shell Command\\*" display-buffer-no-window))))
+    (async-shell-command "cd c:/Users/26289/hugo-doks && git add . && git commit -m 'update' && git push")))
 
 (defun gk/deploy-langc ()
   "Deploy geekblog."
   (interactive)
   (let ((display-buffer-alist
          '(("\\*Async Shell Command\\*" display-buffer-no-window))))
-    (async-shell-command "cd C:/Users/26289/LangC && sh ./deploy.sh")))
+    (async-shell-command "cd C:/Users/26289/LangC && git add . && git commit -m 'update' && git push")))
 
 (defun gk/deploy-gknows ()
   "Deploy geekblog."
@@ -38,6 +52,20 @@
   (let ((display-buffer-alist
          '(("\\*Async Shell Command\\*" display-buffer-no-window))))
     (async-shell-command "cd C:/Users/26289/gknows && git add . && git commit -m 'update' && git push")))
+
+(defun gk/pull-all ()
+  (interactive)
+  (let ((display-buffer-alist
+         '(("\\*Async Shell Command\\*" display-buffer-no-window))))
+    (async-shell-command "cd C:/Users/26289/gknows && git pull")
+    (async-shell-command "cd c:/Users/26289/Hackings/mygtd && git pull")
+    (async-shell-command "cd C:/Users/26289/LangC && git pull")))
+
+(defun gk/push-all ()
+  (interactive)
+  (gk/deploy-mygtd)
+  (gk/deploy-langc)
+  (gk/deploy-gknows))
 
 (use-package command-log-mode
   :ensure t)
@@ -422,180 +450,25 @@ Version 2019-11-04"
 
 (require 'dired)
 (define-key dired-mode-map (kbd "<C-return>") 'xah-open-in-external-app)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Daily page to blog
+(defvar gk-blog-page-dir "c:/Users/26289/geekblog")
 
+(defun gk-daily-page-to-blog ()
+  (interactive)
+  (let* ((title (format-time-string "%b %d, %Y"))
+         (file (expand-file-name (gkroam--get-page title) gkroam-root-dir))
+         content)
+    (with-current-buffer (find-file-noselect file)
+      (save-excursion
+        (goto-char (point-min))
+        (if (re-search-forward (concat "^#\\+TITLE: " title) nil t)
+            (setq content (string-trim (buffer-substring-no-properties
+                                        (1+ (point)) (point-max))))
+          (user-error "Dismatched daily page title, please check!"))))))
 
-;;; verisbilling ifield to ofield.
+(gk-daily-page-to-blog)
 
-;; (defvar vb-ifileds nil)
-
-;; (defvar vb-ofileds nil)
-
-;; (defvar vb-fetch-buffer "*VB Fetch*")
-
-;; (defvar vb-field-pairs
-;;   '(("user_id" . "inst_id")
-;;     ("start_date" . "valid_date")
-;;     ("so_date" . "commit_date"))
-;;   "Matched ifield and ofield pairs.")
-
-;; (defun vb-find-ifield (ofield)
-;;   "Find ifield according to OFIELD in `vb-field-pairs'."
-;;   (car (seq-find (lambda (pair)
-;;                    (string= (cdr pair) ofield))
-;;                  vb-field-pairs)))
-
-;; (defun vb-get-iNo (ifield)
-;;   "return the serial number of IFIELD in `vb-ifileds'."
-;;   (when-let ((pos (seq-position vb-ifileds ifield)))
-;;     (1+ pos)))
-
-;; (defun vb-trans-info (ifield ofield iNo oNo)
-;;   (format "%s %s ------ %s > %s" iNo oNo ifield ofield))
-
-;; (defun vb-trans-field (ifields ofields)
-;;   "Return the result of matching string."
-;;   (let ((res ""))
-;;     (dotimes (i (length ofields))
-;;       (let ((oNo (1+ i))
-;;             (ofield (nth i ofields)))
-;;         (pcase ofield
-;;           ((pred vb-find-ifield)
-;;            (let* ((ifield (vb-find-ifield ofield))
-;;                   (iNo (vb-get-iNo ifield)))
-;;              (setq res (concat res (vb-trans-info ifield ofield iNo oNo) "\n"))))
-;;           ((and (let iNo (vb-get-iNo ofield))
-;;                 (guard iNo))
-;;            (setq res (concat res (vb-trans-info ofield ofield iNo oNo) "\n")))
-;;           (_ (setq res (concat res (vb-trans-info "null" ofield " " oNo) "\n"))))))
-;;     res))
-
-;; (define-minor-mode vb-fetch-mode
-;;   "Minor mode for fetching ifileds and ofields
-;; in `vb-fetch-buffer' buffer."
-;;   nil nil
-;;   (let ((map (make-sparse-keymap)))
-;;     (define-key map (kbd "C-c C-c") #'vb-fetch-finalize)
-;;     (define-key map (kbd "C-c C-k") #'vb-fetch-cancel)
-;;     map)
-;;   (if vb-fetch-mode
-;;       (setq header-line-format "Press 'C-c C-c' to finish, 'C-c C-k' to cancel.")
-;;     (setq header-line-format nil)))
-
-;; (defun vb-fetch-finalize ()
-;;   (interactive)
-;;   (save-excursion
-;;     (goto-char (point-min))
-;;     (let ((count 0))
-;;       (while (re-search-forward "^;; -+" nil t)
-;;         (setq count (1+ count))
-;;         (let* ((beg (point))
-;;                (end (save-excursion
-;;                       (if (re-search-forward "^;; -+" nil t)
-;;                           (progn
-;;                             (forward-line -1)
-;;                             (point))
-;;                         (point-max))))
-;;                (_ (message "beg,end: %s %s"  beg end))
-;;                (str (string-trim (buffer-substring-no-properties beg end)))
-;;                (lst (split-string str "[\n]+")))
-;;           (pcase count
-;;             (1 (setq vb-ifileds lst)
-;;                (message "ifileds:%S" lst))
-;;             (2 (setq vb-ofileds lst)
-;;                (message "ofileds:%S" lst))
-;;             (3 (setq vb-field-pairs
-;;                      (mapcar (lambda (str)
-;;                                (cons (car (split-string str))
-;;                                      (cadr (split-string str))))
-;;                              lst))
-;;                (message "io pairs:%S" vb-field-pairs)))))))
-;;   (pop-to-buffer "*VB Result*")
-;;   (erase-buffer)
-;;   (insert (vb-trans-field vb-ifileds vb-ofileds)))
-
-;; (defun vb-fetch-cancel ()
-;;   (interactive)
-;;   (kill-buffer vb-fetch-buffer))
-
-;; (defun vb-fetch-all-vars ()
-;;   (interactive)
-;;   (let ((buf (get-buffer-create vb-fetch-buffer)))
-;;     (switch-to-buffer buf)
-;;     (vb-fetch-mode)
-;;     (insert ";; 1.请在下面输入所有 'ifield' 字段名，每个字段一行：
-;; ;; -----------------------------------\n\n")
-;;     (insert ";; 2.请在下面输入所有 'ofield' 字段名，每个字段一行：
-;; ;; -----------------------------------\n\n")
-;;     (insert ";; 3.请在下面输入所有不同的 'ifield ofiled' 匹配，每个匹配一行：
-;; ;; -----------------------------------\n")))
-
-;; ;; test
-
-;; ;; (setq vb-ifileds '("CUST_ID" "GROUP_ID" "GROUP_CUST_NAME" "GROUP_TYPE"
-;; ;;                    "MEMBER_CUST_ID" "USECUST_ID" "USER_ID"
-;; ;;                    "REMOVE_TAG" "JOIN_DATE" "REMOVE_DATE"
-;; ;;                    "MODIFY_TAG" "EVENT_CODE" "UPDATE_TIME"))
-
-;; ;; (setq vb-ofileds '("CUST_ID" "GROUP_ID" "GROUP_TYPE"
-;; ;;                    "MEMBER_CUST_ID" "USECUST_ID" "USER_ID"
-;; ;;                    "REMOVE_TAG" "VALID_DATE" "EXPIRE_DATE"
-;; ;;                    "OPER_TYPE" "SO_NBR" "COMMIT_DATE"))
-
-;; ;; (setq vb-field-pairs '(("JOIN_DATE" . "VALID_DATE")
-;; ;;                        ("REMOVE_DATE" . "EXPIRE_DATE")
-;; ;;                        ("MODIFY_TAG" . "OPER_TYPE")
-;; ;;                        ("EVENT_CODE" . "SO_NBR")
-;; ;;                        ("UPDATE_TIME" . "COMMIT_DATE")))
-
-;; ;; (vb-trans-field vb-ifileds vb-ofileds)
-
-;; ;; 1.请在下面输入所有 'ifield' 字段名，每个字段一行：
-;; ;; -----------------------------------
-;; ;; CUST_ID
-;; ;; GROUP_ID
-;; ;; GROUP_CUST_NAME
-;; ;; GROUP_TYPE
-;; ;; MEMBER_BELONG
-;; ;; MEMBER_KIND
-;; ;; MEMBER_CUST_ID
-;; ;; USECUST_ID
-;; ;; USER_ID
-;; ;; SERIAL_NUMBER
-;; ;; NET_TYPE_CODE
-;; ;; CUST_NAME
-;; ;; USECUST_NAME
-;; ;; VPMN_GROUP_ID
-;; ;; JOIN_TYPE
-;; ;; JOIN_DATE
-;; ;; JOIN_STAFF_ID
-;; ;; JOIN_DEPART_ID
-;; ;; REMOVE_TAG
-;; ;; REMOVE_DATE
-;; ;; REMOVE_STAFF_ID
-;; ;; REMOVE_REASON
-;; ;; UPDATE_TIME
-;; ;; ;; 2.请在下面输入所有 'ofield' 字段名，每个字段一行：
-;; ;; ;; -----------------------------------
-;; ;; CUST_ID
-;; ;; GROUP_ID
-;; ;; GROUP_TYPE
-;; ;; MEMBER_CUST_ID
-;; ;; USECUST_ID
-;; ;; USER_ID
-;; ;; REMOVE_TAG
-;; ;; VALID_DATE
-;; ;; EXPIRE_DATE
-;; ;; OPER_TYPE
-;; ;; SO_NBR
-;; ;; COMMIT_DATE
-;; ;; REMARK
-;; ;; ;; 3.请在下面输入所有不同的 'ifield ofiled' 匹配，每个匹配一行：
-;; ;; ;; -----------------------------------
-;; ;; JOIN_DATE VALID_DATE
-;; ;; REMOVE_DATE EXPIRE_DATE
-;; ;; MODIFY_TAG OPER_TYPE
-;; ;; event_code SO_NBR
-;; ;; UPDATE_TIME COMMIT_DATE
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'init-utils)
 ;; ;;; init-utils.el ends here
